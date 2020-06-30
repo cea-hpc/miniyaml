efa2e8243e67bbe6505ec643c305e839a21d66e4
Quentin Bouget <quentin.bouget@cea.fr>
2020-06-30 12:18:40 +0000

    tests: add unit tests for yaml_emit_document_{start,end}()
    Signed-off-by: Quentin Bouget <quentin.bouget@cea.fr>
    Change-Id: I762fcdf6b3b53dde40c6cc6b3c9aead7577173e8

--- tests/unit/check_emit.c
+++ tests/unit/check_emit.c
@@ -49,6 +49,34 @@ START_TEST(yestre_basic)
 }
 END_TEST
 
+/*----------------------------------------------------------------------------*
+ |                           yaml_emit_document_*()                           |
+ *----------------------------------------------------------------------------*/
+
+START_TEST(yed_basic)
+{
+    const char EXPECTED_OUTPUT[] = "--- ~\n"
+                                   "...\n";
+    unsigned char output[sizeof(EXPECTED_OUTPUT)] = {};
+    size_t written = 0;
+
+    yaml_emitter_set_output_string(&emitter, output, sizeof(output), &written);
+
+    ck_assert(yaml_emit_stream_start(&emitter, YAML_UTF8_ENCODING));
+
+    ck_assert(yaml_emit_document_start(&emitter));
+    /* Empty documents are not allowed */
+    ck_assert(yaml_emit_null(&emitter));
+    ck_assert(yaml_emit_document_end(&emitter));
+
+    ck_assert(yaml_emit_stream_end(&emitter));
+    ck_assert(yaml_emitter_flush(&emitter));
+
+    ck_assert_uint_eq(written, sizeof(output) - 1);
+    ck_assert_str_eq((char *)output, EXPECTED_OUTPUT);
+}
+END_TEST
+
 static Suite *
 unit_suite(void)
 {
@@ -60,6 +88,12 @@ unit_suite(void)
     tests = tcase_create("yaml_emit_stream_*");
     tcase_add_checked_fixture(tests, emitter_init, emitter_exit);
     tcase_add_test(tests, yestre_basic);
+
+    suite_add_tcase(suite, tests);
+
+    tests = tcase_create("yaml_emit_document_*");
+    tcase_add_checked_fixture(tests, emitter_init, emitter_exit);
+    tcase_add_test(tests, yed_basic);
 
     suite_add_tcase(suite, tests);
 
