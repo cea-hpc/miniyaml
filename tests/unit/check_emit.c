14d5fc06f5e9eb94944fcdd870f8003596088ffc
Quentin Bouget <quentin.bouget@cea.fr>
2020-06-30 12:21:33 +0000

    tests: add unit tests for yaml_emit_sequence{start,end}()
    Signed-off-by: Quentin Bouget <quentin.bouget@cea.fr>
    Change-Id: Ic8a0edbc27436796120e27a4a57cd59a6886052f

--- tests/unit/check_emit.c
+++ tests/unit/check_emit.c
@@ -134,6 +134,62 @@ START_TEST(yem_tagged)
 }
 END_TEST
 
+/*----------------------------------------------------------------------------*
+ |                           yaml_emit_sequence_*()                           |
+ *----------------------------------------------------------------------------*/
+
+START_TEST(yese_no_tag)
+{
+    const char EXPECTED_OUTPUT[] = "---\n"
+                                   "- ~\n"
+                                   "...\n";
+    unsigned char output[sizeof(EXPECTED_OUTPUT)] = {};
+    size_t written = 0;
+
+    yaml_emitter_set_output_string(&emitter, output, sizeof(output), &written);
+
+    ck_assert(yaml_emit_stream_start(&emitter, YAML_UTF8_ENCODING));
+    ck_assert(yaml_emit_document_start(&emitter));
+
+    ck_assert(yaml_emit_sequence_start(&emitter, NULL));
+    ck_assert(yaml_emit_null(&emitter));
+    ck_assert(yaml_emit_sequence_end(&emitter));
+
+    ck_assert(yaml_emit_document_end(&emitter));
+    ck_assert(yaml_emit_stream_end(&emitter));
+    ck_assert(yaml_emitter_flush(&emitter));
+
+    ck_assert_uint_eq(written, sizeof(output) - 1);
+    ck_assert_str_eq((char *)output, EXPECTED_OUTPUT);
+}
+END_TEST
+
+START_TEST(yese_tagged)
+{
+    const char EXPECTED_OUTPUT[] = "--- !test\n"
+                                   "- ~\n"
+                                   "...\n";
+    unsigned char output[sizeof(EXPECTED_OUTPUT)] = {};
+    size_t written = 0;
+
+    yaml_emitter_set_output_string(&emitter, output, sizeof(output), &written);
+
+    ck_assert(yaml_emit_stream_start(&emitter, YAML_UTF8_ENCODING));
+    ck_assert(yaml_emit_document_start(&emitter));
+
+    ck_assert(yaml_emit_sequence_start(&emitter, "!test"));
+    ck_assert(yaml_emit_null(&emitter));
+    ck_assert(yaml_emit_sequence_end(&emitter));
+
+    ck_assert(yaml_emit_document_end(&emitter));
+    ck_assert(yaml_emit_stream_end(&emitter));
+    ck_assert(yaml_emitter_flush(&emitter));
+
+    ck_assert_uint_eq(written, sizeof(output) - 1);
+    ck_assert_str_eq((char *)output, EXPECTED_OUTPUT);
+}
+END_TEST
+
 static Suite *
 unit_suite(void)
 {
@@ -158,6 +214,13 @@ unit_suite(void)
     tcase_add_checked_fixture(tests, emitter_init, emitter_exit);
     tcase_add_test(tests, yem_no_tag);
     tcase_add_test(tests, yem_tagged);
+
+    suite_add_tcase(suite, tests);
+
+    tests = tcase_create("yaml_emit_sequence_*");
+    tcase_add_checked_fixture(tests, emitter_init, emitter_exit);
+    tcase_add_test(tests, yese_no_tag);
+    tcase_add_test(tests, yese_tagged);
 
     suite_add_tcase(suite, tests);
 
